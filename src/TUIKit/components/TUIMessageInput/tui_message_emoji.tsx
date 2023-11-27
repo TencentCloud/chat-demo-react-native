35b23de09de219da72f1a6fe3e96ab6793f87c07
Jingfeng <joyxian@tencent.com>
2023-11-27 15:47:50 +0800

    fix: emoji display issue

--- src/TUIKit/components/TUIMessageInput/tui_message_emoji.tsx
+++ src/TUIKit/components/TUIMessageInput/tui_message_emoji.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React from "react";
 import {
   Animated,
   LayoutChangeEvent,
@@ -8,13 +8,13 @@ import {
   View,
   ViewStyle,
   useWindowDimensions,
-} from 'react-native';
-import {styles} from './styles';
-import {emojiData} from './emoji_data';
-import {FlatList} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {Button, Image} from '@rneui/themed';
-import {Pressable} from 'react-native';
+} from "react-native";
+import { styles } from "./styles";
+import { emojiData } from "./emoji_data";
+import { FlatList } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Button, Image } from "@rneui/themed";
+import { Pressable } from "react-native";
 
 interface TUIEmojiPanelProps {
   onEmojiSelect: (emoji: string) => void;
@@ -31,7 +31,8 @@ export const TUIMessageEmoji = (props: TUIMessageEmojiProps) => {
   return (
     <Animated.View
       style={[styles.absolute, props.style]}
-      onLayout={props.onLayout}>
+      onLayout={props.onLayout}
+    >
       <EmojiPanel
         onEmojiDelPress={props.onEmojiDelPress}
         onEmojiSelect={props.onEmojiSelect}
@@ -42,13 +43,13 @@ export const TUIMessageEmoji = (props: TUIMessageEmojiProps) => {
 };
 
 const EmojiPanel = React.memo((props: TUIEmojiPanelProps) => {
-  const {bottom} = useSafeAreaInsets();
-  const {onEmojiDelPress, onEmojiSelect, onMessageSendPress} = props;
+  const { bottom } = useSafeAreaInsets();
+  const { onEmojiDelPress, onEmojiSelect, onMessageSendPress } = props;
   const windowWidth = useWindowDimensions().width;
   const getData = () => {
     const rowCount = Math.floor(windowWidth / 40);
     const result = emojiData.reduce<
-      Array<Array<{name: string; unicode: number}>>
+      Array<Array<{ name: string; unicode: number }>>
     >((acc, cur, i) => {
       const index = Math.floor(i / rowCount); // 计算当前元素应该放在哪个子数组中
       if (!acc[index]) {
@@ -61,28 +62,30 @@ const EmojiPanel = React.memo((props: TUIEmojiPanelProps) => {
   };
 
   const charFromUtf16 = (utf16: any) => {
-    return String.fromCodePoint(...utf16.split('-').map((u: any) => '0x' + u));
+    return String.fromCodePoint(...utf16.split("-").map((u: any) => "0x" + u));
   };
 
-  const rederItem: ListRenderItem<
-    Array<{name: string; unicode: number}>
-  > = ceil => {
+  const rederItem: ListRenderItem<Array<{ name: string; unicode: number }>> = (
+    ceil
+  ) => {
     return (
       <View
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-        }}>
-        {ceil.item.map(item => {
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        {ceil.item.map((item) => {
           const displayText = charFromUtf16(item.unicode.toString(16));
           return (
             <Pressable
               key={item.name}
               onPress={() => {
                 onEmojiSelect(displayText);
-              }}>
-              <View style={{width: 40, height: 40}}>
-                <Text style={{fontSize: 26}}>{displayText}</Text>
+              }}
+            >
+              <View style={{ width: 40, height: 40 }}>
+                <Text style={{ fontSize: 26, color: 'black'  }}>{displayText}</Text>
               </View>
             </Pressable>
           );
@@ -98,7 +101,8 @@ const EmojiPanel = React.memo((props: TUIEmojiPanelProps) => {
         {
           paddingBottom: bottom,
         },
-      ]}>
+      ]}
+    >
       <FlatList
         showsVerticalScrollIndicator={false}
         data={getData()}
@@ -110,16 +114,17 @@ const EmojiPanel = React.memo((props: TUIEmojiPanelProps) => {
           {
             bottom: bottom,
           },
-        ]}>
+        ]}
+      >
         <Image
-          source={require('../../../assets/delete_emoji.png')}
+          source={require("../../../assets/delete_emoji.png")}
           style={styles.delImg}
           onPress={onEmojiDelPress}
         />
         <Button
           title="发送"
           buttonStyle={styles.sendBtn}
-          titleStyle={{fontSize: 12}}
+          titleStyle={{ fontSize: 12 }}
           containerStyle={{
             marginLeft: 10,
           }}
